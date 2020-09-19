3cf5e00146083f5e5f489580e9b23873a87e4a82
rmadukayil <rmadukayil@gmail.com>
2020-09-19 00:12:30 -0400

    Added material UI imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import {Card, CardContent, FormControl, MenuItem, Select} from "@material-ui/core";
 import './App.css';
 
 function App() {
   return (
     <div className="app"> 
-    <h1>
-      Building our COVID 19 tracker
-    </h1>
+    <h1>COVID-19 Tracker</h1>
+
+    <FormControl className="app_dropdown">
+      <Select variant = "outlined" value = "abc">
+        <MenuItem value = "WorldWide">Worldwide</MenuItem> 
+        <MenuItem value = "WorldWide">Option2</MenuItem>
+        <MenuItem value = "WorldWide">3</MenuItem> 
+        <MenuItem value = "WorldWide">4</MenuItem> 
+
+      </Select>
+    </FormControl>
     </div>
   );
 }
