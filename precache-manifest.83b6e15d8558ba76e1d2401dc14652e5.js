112c5f4b2af972f7d18ed76527351c401aa8e9d5
rmadukayil <rmadukayil@gmail.com>
2020-09-20 16:51:38 -0400

    Updates

---
+++ precache-manifest.83b6e15d8558ba76e1d2401dc14652e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f52bbf1ed9c1cd73a72094ba79374a",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "decf51e79cd9a9255330",
+    "url": "/covid-19-tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "b8d1bf4f236064704837",
+    "url": "/covid-19-tracker/static/css/main.ec99a01f.chunk.css"
+  },
+  {
+    "revision": "decf51e79cd9a9255330",
+    "url": "/covid-19-tracker/static/js/2.47e8cebf.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/covid-19-tracker/static/js/2.47e8cebf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8d1bf4f236064704837",
+    "url": "/covid-19-tracker/static/js/main.53b64256.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  }
+]);
